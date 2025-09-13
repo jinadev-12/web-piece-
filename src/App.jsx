595e96aab09041ecf1fb02b5ef38efb4cec83656
Jinadev <jinad3v@gmail.com>
2025-09-13 17:43:43 +0530

    starter file

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react'
+import './index.css'
+import Sidebar from './Sidebar'
 
-import {
-  GsapFrom,
-  GsapFromTo,
-  GsapScrollTrigger,
-  GsapStagger,
-  GsapText,
-  GsapTimeline,
-  GsapTo,
-  Home,
-} from "./pages";
 
-const App = () => {
+
+function App() {
   return (
-    <div className="bg-black min-h-screen w-full">
-      <Router>
-        <Routes>
-          <Route path="/gsapto" element={<GsapTo />} />
-          <Route path="/gsapfrom" element={<GsapFrom />} />
-          <Route path="/gsapfromto" element={<GsapFromTo />} />
-          <Route path="/gsaptimeline" element={<GsapTimeline />} />
-          <Route path="/gsapstagger" element={<GsapStagger />} />
-          <Route path="/gsapscrolltrigger" element={<GsapScrollTrigger />} />
-          <Route path="/gsaptext" element={<GsapText />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+    <div>
+      <h1 className="bg-blue-700">fdsfhklsdhf</h1>
+      <br />
+      <Sidebar />
     </div>
   );
-};
+}
 
-export default App;
+export default App
